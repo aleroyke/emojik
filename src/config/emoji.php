0f6e523825e85bdce9aecaa6bc04e69fc95c753b
Antoine 'Kwd' LRK <a.leroyke@panthera.studio>
2021-06-27 11:09:58 +0200

    Started Unicode 12.0 2019

--- src/config/emoji.php
+++ src/config/emoji.php
@@ -3,14 +3,14 @@
 namespace PantheraStudio\Emojik;
 
 return [
-    // Skin tone
+    // -- Skin tone -- //
     ":light_skin_tone:" => "\u{1F3FC}",
     ":medium_light_skin_tone:" => "\u{1F3FC}",
     ":medium_skin_tone:" => "\u{1F3FD}",
     ":medium_dark_skin_tone:" => "\u{1F3FE}",
     ":dark_skin_tone:" => "\u{1F3FF}",
 
-    // Emoji list
+    // -- Emoji list -- //
     ":grinning_face:" => "\u{1F600}",
     ":smiling_face_with_hearts:" => "\u{1F970}",
     ":waving_hand:" => "\u{1F44B}",
@@ -24,6 +24,21 @@ return [
     ":person_getting_massage:" => "\u{1F486}",
     ":heart_exclamation:" => "\u{2763}\u{FE0F}",
     //-- 26/06/2021 --//
-    "sunglasses" => "\u{1F60E}",
-    "frog" => "\u{1F438}"
+    ":sunglasses:" => "\u{1F60E}",
+    ":frog:" => "\u{1F438}",
+
+    // -- Unicode 12.0 (2019) -- //
+    ":adhesive_bandage:" => "\u{1FA79}",
+    ":auto_rickshaw:" => "\u{1F6FA}",
+    ":axe:" => "\u{1FA93}",
+    ":ballet_shoes:" => "\u{1FA70}",
+    ":banjo:" => "\u{1FA95}",
+    ":beverage_box:" => "\u{1F9C3}",
+    ":blue_square:" => "\u{1F7E6}",
+    ":brown_circle:" => "\u{1F7E4}",
+    ":brown_heart:" => "\u{1F90E}",
+    ":brown_square:" => "\u{1F7EB}",
+    ":butter:" => "\u{1F9C8}",
+    ":chair:" => "\u{1FA91}",
+    ":deaf_person:" => "\u{1F9CF}",
 ];
